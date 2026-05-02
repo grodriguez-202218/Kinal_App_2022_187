a00f78726ed81236c37fc7de09f4d6a81cdb3968
grodriguez-2022187 <grodriguez-2022187@kinal.edu.gt>
2026-05-01 19:39:23 -0600

    refactor: finalizar lógica de encriptación en PasswordMigrationUtil

--- src/main/java/com/gahelrodriguez/kinalapp/util/PasswordMigrationUtil.java
+++ src/main/java/com/gahelrodriguez/kinalapp/util/PasswordMigrationUtil.java
@@ -1,4 +1,23 @@
 package com.gahelrodriguez.kinalapp.util;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordMigrationUtil {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        // Agrega aquí las contraseñas de texto plano que necesites hashear
+        String[] passwordsPlanas = {"1234", "admin", "vendedor", "user123"};
+
+        System.out.println("=== Hashes BCrypt generados ===");
+        System.out.println();
+
+        for (String password : passwordsPlanas) {
+            String hash = encoder.encode(password);
+            System.out.println("Texto plano : " + password);
+            System.out.println("Hash BCrypt : " + hash);
+            System.out.println("SQL UPDATE  : UPDATE usuarios SET password = '" + hash + "' WHERE password = '" + password + "';");
+            System.out.println();
+        }
+    }
 }
